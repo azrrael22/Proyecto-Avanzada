55866869ad6c9e0c221cdbdd09181a6493f942a5
Adriaann <adriaannm.rodriguezr@uqvirtual.edu.co>
2025-09-01 14:26:01 -0500

    Creación del proyecto

---
+++ src/main/java/uniquindio/edu/co/Proyecto_Avanzada/ProyectoAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package uniquindio.edu.co.Proyecto_Avanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAvanzadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uniquindio/edu/co/Proyecto_Avanzada/ProyectoAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package uniquindio.edu.co.Proyecto_Avanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAvanzadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
